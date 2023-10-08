d61c2dce92943b4449302930f99a573df85b6ba9
Isaias Mendes <isaiaszmendes@gmail.com>
2023-10-07 22:35:41 -0300

    add use case test to product

---
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,26 @@
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  }
+}
+
+describe('Add Product useCase unit test', () => { 
+  it('should add a product', () => {
+    // repositorio
+    // usecase
+    const productRepository = MockRepository()
+    const useCase = new AddProductUseCase(productRepository)
+
+    const input = {
+      name: 'Product 1',
+      description: 'Product 1 description',
+      purchasePrice: 100,
+      stock: 10,
+    }
+
+    useCase.execute(input)
+    // input
+    // output
+  })
+})
