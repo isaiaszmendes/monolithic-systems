f403ff32f0c8956991192e215f43ccc1481921d1
Isaias Mendes <isaiaszmendes@gmail.com>
2023-10-25 23:13:59 -0300

    add test ProductAdmFacade factory

---
+++ src/modules/product-adm/factory/facade.factory.spec.ts
@@ -0,0 +1,46 @@
+import { Sequelize } from "sequelize-typescript";
+import { ProductModel } from "../repository/product.model";
+import { ProductAdmFacadeFactory } from "./facade.factory";
+
+describe('ProductAdmFacade Factory Test', () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+		sequelize = new Sequelize({
+			dialect: 'sqlite',
+			storage: ':memory:',
+			logging: false,
+			sync: { force: true },
+		});
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+	afterEach(async () => {
+		await sequelize.close();
+	});
+
+  it('should create a product', async () => {
+    const productFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: '1',
+      name: 'product name',
+      description: 'product description',
+      purchasePrice: 10,
+      stock: 10,
+    }
+
+    await productFacade.addProduct(input);
+
+    const product = await ProductModel.findOne({ where: { id: input.id } });
+
+    expect(product).toBeDefined();
+    expect(product.id).toBe(input.id);
+    expect(product.name).toBe(input.name);
+    expect(product.description).toBe(input.description);
+    expect(product.purchasePrice).toBe(input.purchasePrice);
+    expect(product.stock).toBe(input.stock);
+  });
+})
