e85e4075b23b03fd38e12ae756ae03649e4f7dba
Isaias Mendes <isaiaszmendes@gmail.com>
2024-03-24 01:33:09 -0300

    add check stock useCase

---
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.ts
@@ -0,0 +1,15 @@
+import { CheckStockFacadeOutputDTO } from "../../facade/product-adm.facade.interface";
+import { ProductGateway } from "../../gateway/product.gateway";
+import { CheckStockInputDTO } from "./check-stock.dto";
+
+export class CheckStockUseCase {
+  constructor(private readonly productRepository: ProductGateway) {}
+
+  async execute(input: CheckStockInputDTO): Promise<CheckStockFacadeOutputDTO> {
+    const product = await this.productRepository.find(input.productId);
+    return {
+      productId: product.id.id,
+      stock: product.stock,
+    };
+  }
+}
