d0459afab495b23e790cdc35a9589dff6e87557a
Isaias Mendes <isaiaszmendes@gmail.com>
2024-03-24 02:20:22 -0300

    implementes useCaseInterface

--- src/modules/product-adm/usecase/check-stock/check-stock.usecase.ts
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.ts
@@ -1,8 +1,9 @@
+import { UseCaseInterface } from "../../../@shared/usecase/use-case.interface";
 import { CheckStockFacadeOutputDTO } from "../../facade/product-adm.facade.interface";
 import { ProductGateway } from "../../gateway/product.gateway";
 import { CheckStockInputDTO } from "./check-stock.dto";
 
-export class CheckStockUseCase {
+export class CheckStockUseCase implements UseCaseInterface  {
   constructor(private readonly productRepository: ProductGateway) {}
 
   async execute(input: CheckStockInputDTO): Promise<CheckStockFacadeOutputDTO> {
