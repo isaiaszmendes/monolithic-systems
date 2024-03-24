7622cd2a1b4e46c83eef2c21d8feb3e93a78cb81
Isaias Mendes <isaiaszmendes@gmail.com>
2024-03-24 04:04:29 -0300

    add facade factory to catalog module

---
+++ src/modules/store-catalog/factory/facade.factory.ts
@@ -0,0 +1,20 @@
+import { StoreCatalogFacade } from "../facade/store-catalog.facade";
+import { StoreCatalogFacadeInterface } from "../facade/store-catalog.facade.interface";
+import { ProductRepository } from "../repository/product.repository";
+import { FindAllProductsUseCase } from "../usecase/find-all-products/find-all-products.useCase";
+import { FindProductUseCase } from "../usecase/find-product/find-product.usecase";
+
+export class StoreCatalogFacadeFactory {
+  static create(): StoreCatalogFacade {
+    const productRepository = new ProductRepository();
+    const findUseCase = new FindProductUseCase(productRepository);
+    const findAllUseCase = new FindAllProductsUseCase(productRepository);
+
+    const facade = new StoreCatalogFacade({
+      findUseCase: findUseCase,
+      findAllUseCase: findAllUseCase
+    })
+
+    return facade;
+  }
+}
